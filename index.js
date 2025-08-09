3288aa370d94385cef89ccdacd446b344a1413d7
Pedro Alejandro Cubias Henriquez <00026623@uca.edu.sv>
2025-08-09 02:23:28 -0600

    Regreso a commit 1

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
 // index.js
 import './bot.js'; 
-sock.ev.on('connection.update', (update) => {
-  if (update.connection === 'open') {
-    const fullId = update?.me?.id || '';
-    const botId = fullId.split(':')[0] + '@s.whatsapp.net';
-    console.log('ID real del bot:', botId);
-  }
-});
